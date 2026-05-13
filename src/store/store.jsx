e5ac2eb58e3bfb061ab9472b536b1c5befd63d4d
Yusuf Saiyad <yusufsaiyed66@gmail.com>
2026-05-13 17:57:18 +0530

    Performing Redux with real world example

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
-import  counterReducer  from "./reducers/counterSlice";
+ import { configureStore } from "@reduxjs/toolkit";
+import UserReducer from "./reducers/UserReducer";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
+
+ export const store = configureStore({
+  reducer:{
+    UserReducer:UserReducer 
   },
-});
+ });        
